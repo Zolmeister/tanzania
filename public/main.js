a1f89be9a36669f8059d6e0dbc30b4d2faf9e21c
Zolmeister <zolikahan@gmail.com>
2017-01-16 17:27:37 -0600

    fix artifacting on high-res displays

--- public/main.js
+++ public/main.js
@@ -14,7 +14,7 @@
   var started = false
   var zoom = 0
   var debug = false
-  
+
   // only log when in debug mode
   var log = debug ? console.log.bind(console) : function(){}
 
@@ -37,7 +37,7 @@
     $('#sidebar-taz')[0].scrollTop = 0
     $('#start-button-taz').bind('click', start)
     $('#fullscreen-button-taz').bind('click', fullscreen)
-    
+
     // Reset app on fullscreen change
     $(window).bind('webkitfullscreenchange mozfullscreenchange', function () {
       $('#container-taz canvas').remove()
@@ -45,11 +45,11 @@
       started = false
       updateSidebarImages()
     }).bind('beforeunload', function () {
-      
+
       // Fixes Firefox sidebar scroll sticking issue
       $('#sidebar-taz')[0].scrollTop = 0
     })
-    
+
     initSidebar()
   })
 
@@ -98,7 +98,7 @@
     log('updating sidebar images')
     var $sidebar = $('#sidebar-taz')
     var sideBarScroll = $sidebar.scrollTop()
-    
+
     // Get viewable area
     var viewBox = {
       top: sideBarScroll,
@@ -108,7 +108,7 @@
     // Buffer next few non-visible elements for a smoother experience
     var doNext = 0
     debug && console.time('sidebar')
-    
+
     // Check sidebar elements to see if they are visible and need to be rendered
     for (i = 0; i < $thumbnails.length; i++) {
       var $el = $thumbnails[i]
@@ -140,7 +140,7 @@
     }
     debug && console.timeEnd('sidebar')
   }
-  
+
   /* **** These functions control the overlay before starting **** */
   function initOverlay() {
     $('#overlay-taz').show()
@@ -181,7 +181,7 @@
       selectImage(currentPic - 1)
     }
   }
-  
+
   function getImageSize() {
     var originalImageWidth = 1624
     var originalImageHeight = 1080
@@ -198,7 +198,7 @@
       height: h
     }
   }
-  
+
   function renderImage() {
     geometry.attributes.color.needsUpdate = true
   }
@@ -208,7 +208,7 @@
       loadImage(num, true, cb)
     }
   }
-  
+
   function selectImage(pic) {
     if (!started || !ready) return;
     log('selecting', pic)
@@ -231,7 +231,7 @@
       })
     })
   }
-  
+
   function loadImage(num, buffer, cb) {
     log('loading image', num)
 
@@ -269,7 +269,7 @@
 
       var ctx = c.getContext('2d')
       ctx.drawImage(img, 0, 0, imgWidth, imgHeight)
-      //document.body.appendChild(c)
+      // document.body.appendChild(c)
       var pixels = ctx.getImageData(0, 0, c.width, c.height).data
       debug && console.time('imageLoad')
 
@@ -327,7 +327,7 @@
     bufferVerticies(getImageSize(), function () {
       log('loading first image')
       loadImage(currentPic, false, function () {
-        
+
         // buffer next image in sequence
         bufferImage(currentPic + 1)
         clearOverlay()
@@ -353,7 +353,7 @@
 
   // generate all of the verticies needed for the mesh 1:1 pixel:vertex ratio
   function bufferVerticies(imgSize, cb) {
-    
+
     // BufferGeometry is MUCH more memory efficient
     // It also allows more efficient web worker usage
     geometry = new THREE.BufferGeometry()
@@ -399,26 +399,28 @@
     // position camera for a 1:1 pixel unit ratio
     var vFOV = camera.fov * (Math.PI / 180) // convert VERTICAL fov to radians
     var targetZ = height / (2 * Math.tan(vFOV / 2))
-    
+
     // add 2 to fix artifacts
     camera.position.z = targetZ + 2
+    // fixed artifacting on high-res displays, but causes zoom-out
+    camera.position.z *= window.devicePixelRatio
 
     scene = new THREE.Scene()
 
     uniforms = {
-      
+
       // This dictates the point of the animation sequence
       amplitude: {
         type: 'f',
         value: 0
       },
-      
+
       // Dictates the transition animation. Ranges from 0-9
       transition: {
         type: 'i',
         value: 0
       },
-      
+
       // Boolean for transition animation changes to deal with higher density
       // 0 or 1
       fullscreen: {
@@ -473,22 +475,22 @@
         if (zDelta <= 0) {
           zDelta -= 1
         }
-        
+
         // Zoom is relative based on fullscreen state due to particle density
         camera.position.z -= zDelta * zoomConstant
         camera.updateProjectionMatriz = true
         render()
         zoom = (zoom + 1) % 50
-        
+
         if (uniforms.amplitude.value <= 0.001) {
           log('animation ended, calling end callback')
-          
+
           // Animate sidebar
           animationEndCallbacks[1](function () {
-            
+
             // Buffer next image
             animationEndCallbacks[0](function () {
-              
+
               // All events are toggled back on to allow for next transition
               ready = true
               log('ready')
@@ -504,7 +506,7 @@
   }
 
   function render() {
-    
+
     // update the amplitude based on the frame
     uniforms.amplitude.value = Math.abs(Math.sin(frame))
     renderer.render(scene, camera)
